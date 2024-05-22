a989ddcf8efba38d267d31519bc6e93ed4b88cd2
Matej Topolovac <>
2024-05-22 21:19:55 +0200

    feat: added basic websocket handler

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
+
+type Message struct {
+	Id   int    `json:"id"`
+	Text string `json:"text"`
+	User string `json:"user"`
+}
 
 func main() {
 	router := mux.NewRouter()
@@ -18,6 +26,39 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
+
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	router.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer conn.Close()
+
+		err = conn.WriteJSON(&Message{
+			Id:   0,
+			Text: "Welcome to the chat!",
+			User: "Server Info",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			var message Message
+			err := conn.ReadJSON(&message)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("new message: %v\n", message)
+		}
+	})
 
 	log.Printf("Server listening on port %s...", server.Addr)
 	log.Fatal(server.ListenAndServe())
