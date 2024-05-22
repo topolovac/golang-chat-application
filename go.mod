f35b8e457458f1966bcad602995ef54103f3bfd6
Matej Topolovac <>
2024-05-22 20:49:10 +0200

    feat: basic server serving index.html

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golang_chat_application
 
 go 1.22.2
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("hello world")
+	router := mux.NewRouter()
+	router.Handle("/", http.FileServer(http.Dir("public")))
+
+	server := &http.Server{
+		Handler:      router,
+		Addr:         ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Printf("Server listening on port %s...", server.Addr)
+	log.Fatal(server.ListenAndServe())
 }
