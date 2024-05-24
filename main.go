d9d8a8489618fc207b9f3302c5e224f0498c2104
Matej Topolovac <>
2024-05-24 11:39:38 +0200

    feat: added register and unregister channels

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -18,12 +19,37 @@ type Message struct {
 type Hub struct {
 	connections []*websocket.Conn
 	broadcast   chan Message
+	unregister  chan *websocket.Conn
+	register    chan *websocket.Conn
+}
+
+func broadcast(hub *Hub, message Message) {
+	for _, conn := range hub.connections {
+		if err := conn.WriteJSON(message); err != nil {
+			fmt.Printf("error writing to connection: %v\n", err)
+		}
+	}
 }
 
 func (h *Hub) startHub() {
-	for message := range h.broadcast {
-		for _, conn := range h.connections {
-			conn.WriteJSON(message)
+	for {
+		select {
+		case message := <-h.broadcast:
+			fmt.Printf("broadcasting message: %v\n", message)
+			broadcast(h, message)
+		case conn := <-h.register:
+			fmt.Println("new connection")
+			broadcast(h, Message{0, "User connected", "Server Info"})
+			h.connections = append(h.connections, conn)
+		case conn := <-h.unregister:
+			fmt.Println("client disconnected")
+			for i, c := range h.connections {
+				if c == conn {
+					h.connections = append(h.connections[:i], h.connections[i+1:]...)
+					break
+				}
+			}
+			broadcast(h, Message{0, "User disconnected", "Server Info"})
 		}
 	}
 }
@@ -47,6 +73,8 @@ func main() {
 	hub := &Hub{
 		connections: []*websocket.Conn{},
 		broadcast:   make(chan Message),
+		register:    make(chan *websocket.Conn),
+		unregister:  make(chan *websocket.Conn),
 	}
 
 	router.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
@@ -57,7 +85,7 @@ func main() {
 
 		defer conn.Close()
 
-		hub.connections = append(hub.connections, conn)
+		hub.register <- conn
 
 		err = conn.WriteJSON(&Message{
 			Id:   0,
@@ -72,7 +100,10 @@ func main() {
 			var message Message
 			err := conn.ReadJSON(&message)
 			if err != nil {
-				log.Fatal(err)
+				fmt.Println("error reading messages")
+				conn.Close()
+				hub.unregister <- conn
+				break
 			}
 
 			hub.broadcast <- message
